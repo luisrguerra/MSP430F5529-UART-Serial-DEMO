bc3ed4a0b6de16f10653c92d1d794d0b0336e8bd
luisrguerra <luiseduardoribeiroguerra@gmail.com>
2022-09-12 10:55:28 -0300

    upload

---
+++ Debug/main.d
@@ -0,0 +1,24 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ccs1120/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ccs1120/ccs/ccs_base/msp430/include/msp430f5529.h
+main.obj: C:/ccs1120/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: C:/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/Users/luise/workspace_v11/uart3/uart.h
+
+../main.c:
+
+C:/ccs1120/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ccs1120/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+C:/ccs1120/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+C:/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/Users/luise/workspace_v11/uart3/uart.h:
+
