37be7eb8a4e7fd570278edcad301d1443063ddb7
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2023-07-14 21:32:11 -0300

    atualizar

---
+++ Debug/uart.d
@@ -0,0 +1,36 @@
+# FIXED
+
+uart.obj: ../uart.c
+uart.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h
+uart.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430f5529.h
+uart.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h
+uart.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+uart.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+uart.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h
+uart.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+uart.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+uart.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+uart.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h
+
+../uart.c:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h:
+
